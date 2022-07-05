948ec3985188d48ed8ddd6f0aec476479989524a
wolottoh <ibuej@yahoo.co.uk>
2022-07-05 09:33:07 +0100

    first commit

---
+++ src/main/java/com/qa/dfespringboot/DfeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfeSpringBootApplication.class, args);
+	}
+
+}
